044c066c7b318233b2cb6da387044bbbc4a934bc
Chenggang Zhao <lyricz@yeah.net>
2022-06-20 10:43:10 +0800

    Add TMUX runner

---
+++ experiments/run_tmux.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+current_path=$(pwd)
+
+# Create windows.
+tmux new -s "Canvas" -d
+tmux selectp -t 1
+tmux splitw -v -p 50
+tmux selectp -t 1
+tmux splitw -v -p 50
+tmux selectp -t 3
+tmux splitw -v -p 50
+
+# Run Canvas.
+for i in {1..4}
+do
+tmux send-keys -t "$i" "echo TMUX Pane $i" Enter
+# You may change to your personal environment.
+tmux send-keys -t "$i" "source ~/.local/miniconda3/etc/profile.d/conda.sh; conda activate Canvas" Enter
+tmux send-keys -t "$i" "cd ${current_path}" Enter
+tmux send-keys -t "$i" "$*" Enter
+done
+
+# Attach.
+tmux attach -t "Canvas"
