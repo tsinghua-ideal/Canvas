554b2782f4da77c0bd495372666ee4fcaec371af
Chenggang Zhao <lyricz@yeah.net>
2022-07-05 17:10:21 +0800

    Better weight initialization policy

--- canvas/utils.py
+++ canvas/utils.py
@@ -1,3 +1,5 @@
+import math
+
 from collections.abc import Iterable
 from typing import Tuple
 from torch import nn
@@ -47,6 +49,8 @@ def init_weights(m):
         nn.init.constant_(m.bias, 0)
         nn.init.constant_(m.weight, 1.0)
     elif isinstance(m, nn.Conv2d):
-        nn.init.trunc_normal_(m.weight.data, std=.02)
+        fan_in = m.kernel_size[0] * m.kernel_size[1] * m.in_channels
+        fan_in //= m.groups
+        m.weight.data.uniform_(-math.sqrt(3.0 / fan_in), math.sqrt(3.0 / fan_in))
         if m.bias is not None:
             m.bias.data.zero_()
