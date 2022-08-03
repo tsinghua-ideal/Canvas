a682b6eeff8fcf6f7c6fa6baf217af892914131f
Chenggang Zhao <lyricz@yeah.net>
2022-08-03 17:13:49 +0800

    Better initialization policy

--- experiments/collections/Canvas_07159_Kernel_16331621907596408686/Kernel_16331621907596408686.py
+++ experiments/collections/Canvas_07159_Kernel_16331621907596408686/Kernel_16331621907596408686.py
@@ -9,20 +9,24 @@ class Kernel_16331621907596408686(nn.Module):
     def __init__(self, c: int, h: int, w: int):
         # Configurations
         super(Kernel_16331621907596408686, self).__init__()
-        self.g = 1
+        self.g, self.r = 4, 4
         self.n, self.c, self.h, self.w = None, c, h, w
-        
+        self.kh, self.kw, self.dh, self.dw = 3, 3, 3, 3
+        self.ph, self.pw = self.dh * ((self.kh - 1) // 2), self.dw * ((self.kw - 1) // 2)
+
         # Kernels
         # Input: p_0
         pass
+        self.reduce = nn.Conv2d(self.c, self.c // self.r, 1)
         # UnfoldH_K3_D3: p_2
         pass
         # Convolution_3_3_2_2_DW0: p_3
-        self.p_3 = nn.Conv2d(self.c, self.c * 3, (3, 3), dilation=(2, 2), padding=(2, 2), groups=self.g)
+        self.p_3 = nn.Conv2d(self.c, self.c * (self.kh * self.kw) // self.r,
+                             (3, 3), dilation=(2, 2), padding=(2, 2), groups=self.g)
         # BMul: p_4
         pass
         # FC: p_7
-        self.p_7 = nn.Conv2d(self.c * 3, self.c, 1, padding=0, groups=self.g)
+        self.p_7 = nn.Conv2d(self.c * (self.kh * self.kw) // self.r, self.c, 1, padding=0, groups=self.g)
         # Output: p_9
         pass
     
@@ -32,15 +36,12 @@ class Kernel_16331621907596408686(nn.Module):
         self.n = t_0.size(0)
         assert (self.n, self.c, self.h, self.w) == tuple(t_0.size())
         # UnfoldH_K3_D3: p_2
-        t_2 = F.unfold(t_0, (3, 1), dilation=(3, 1), padding=(3, 0))
-        t_2 = t_2.view(self.n, self.c, 3, self.h, self.w)
+        t_2 = F.unfold(self.reduce(t_0), (self.kh, self.kw), dilation=(self.dh, self.dw), padding=(self.ph, self.pw))
+        t_2 = t_2.view(self.n, self.c * (self.kh * self.kw) // self.r, self.h, self.w)
         # Convolution_3_3_2_2_DW0: p_3
         t_3 = self.p_3(t_0)
         # BMul: p_4
-        t_4_lhs = t_2.view(self.n, 1, self.c * 3, self.h, self.w)
-        t_4_rhs = t_3.view(self.n, 1, self.c * 3, self.h, self.w)
-        t_4 = t_4_lhs * t_4_rhs
-        t_4 = t_4.view(self.n, self.c * 3, self.h, self.w)
+        t_4 = t_2 * t_3
         # FC: p_7
         t_7 = self.p_7(t_4)
         return t_0 + t_7
