737148abc10dc25043ee55de001f5b56f53cd54d
ShenAo1111 <1377693092@qq.com>
2023-11-03 22:17:52 +0800

    refactor(train): Add the proxyless logic to test the code

--- experiments/train.py
+++ experiments/train.py
@@ -1,21 +1,29 @@
 import torch
 import os, json
-from base import dataset, device, models, parser, trainer, log
+from base import dataset, device, models, parser, trainer, log, proxyless_trainer
 
 
 if __name__ == '__main__':
     # Get arguments
     logger = log.get_logger()
     args = parser.arg_parse()
-    # Check available devices and set distributed
+    
+    # Check available devices, set seed, and setdistributed
     device.initialize(args)
     
     # Training utils
     model = models.get_model(args)
-    train_loader,  eval_loader = dataset.get_loaders(args)
-    
+    if args.needs_valid:
+        train_loader, valid_loader, eval_loader = dataset.get_loaders(args)
+    else:
+        train_loader, eval_loader = dataset.get_loaders(args)
+        
     # Start to train
-    metrics = trainer.train(args, model=model, train_loader=train_loader, eval_loader=eval_loader)
+    if args.needs_valid:
+        metrics = proxyless_trainer.train(args, model=model, train_loader=train_loader, valid_loader=valid_loader, eval_loader=eval_loader)
+    else:
+        metrics = trainer.train(args, model=model, train_loader=train_loader, eval_loader=eval_loader)
+        
     if os.path.exists(args.canvas_log_dir):
             assert os.path.isdir(args.canvas_log_dir), 'Canvas logging path must be a directory'
     dir_name = f'Canvas_warmup_epochs_{args.warmup_epochs}_epochs_{args.epochs}_lr_{args.lr}'
